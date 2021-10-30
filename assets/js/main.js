04eadeec50dbd7be9ef2148df4c5652c41c83e96
Aryan basu <aryanbasu005@gmail.com>
2021-10-30 20:04:03 +0530

    shop database

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,95 @@
 
+var shop=[{
+id=1,
+Name:'Vizylac Capsule',
+Description:'It is used to restore the normal microbial flora (also known as gut microbes) of the intestine. It is used for the treatment and prevention of diarrhea associated with excessive use of antibiotics. It is also used as supportive therapy in the treatment of digestive disorders such as colitis and gastroenteritis. It can also be used in infants for the treatment of enteric infections such as salmonellosis.',
+Price:54,
+inCart:0
 
+},
+{
+id=2,
+Name:'Nodosis - DS Tablet',
+Description:'Nodosis - DS Tablet is used as an alkalinizing agent in the treatment of metabolic acidosis. Useful in metabolic acidosis, gastric, systemic, and urinary alkalinization, severe diarrhea, and chemical phlebitis.',
+Price:77,
+inCart:0
+},
+{
+   id=3,
+   Name:'Himalaya Cystone Forte Tablet',
+   Description:'Himalaya Cystone Forte Tablet is a highly potent medicine for the management of renal stones. Renal stones are more common because of hot and dry weather conditions and dietary habits. It is also used to treat the pain and spasm associated with kidney stones.',
+   Price:90,
+   inCart:0
+   },
+   {
+      id=4,
+      Name:'Allstar Reusable Insulin Pen',
+      Description:'Allstar Reusable Insulin Pen is a reusable pen for the injection of Insulin. Device has a combination of sleek design styling and sophisticated technology and is used to deliver the insulin in the body. AllStar can only be used with Sanofi 3ml (300 units) insulin cartridges (100 units per ml) from Sanofi Group.',
+      Price:762,
+      inCart:0
+      },
+      {
+         id=5,
+         Name:'Bevon -CD Capsule',
+         Description:'Bevon-CD is indicated in Diabetic Neuropathy, Diabetic Nephropathy, Diabetic retinopathy, CVD and Alcoholic Neuropathy. Multiminerals in Bevon-CD maintains a healthy functioning body, and contributes to Antioxidant and nutritional supplement in cardiovascular diseases and diabetes mellitus.',
+         Price:111,
+         inCart:0
+         },
+         {
+            id=6,
+            Name:'Montair-LC Tablet',
+            Description:'Montair-LC Tablet is a combination medicine used in the treatment of allergic symptoms such as runny nose, stuffy nose, sneezing, itching, swelling, watery eyes and congestion or stuffiness. It also reduces inflammation in the airways and make breathing easier.',
+            Price:229,
+            inCart:0
+            },
+            {
+               id=7,
+               Name:'Gemcal Soft Gelatin Capsule',
+               Description:'Gemcal Soft Gelatin Capsule is an excellent dietary and nutritional supplement and is taken for calcium deficiency state which may occur in postmenopausal osteoporosis, rickets and osteomalacia (softening of the bones). Gemcal capsule is a rich source of calcium while Calcitriol is a synthetic vitamin D analog which is active and helps in softening and providing strength to the bones.',
+               Price:225 ,
+               inCart:0
+               },
+               {
+                  id=8,
+                  Name:'Calpol 650mg Tablet',
+                  Description:'Calpol 650mg Tablet is a medicine used to relieve pain and to reduce fever. It is used to treat many conditions such as headaches, body aches, toothaches, and the common cold. It works by inhibiting the release of certain chemicals that cause pain and fever.',
+                  Price: 22,
+                  inCart:0
+                  },
+                  {
+                     id=9,
+                     Name:'Azee 500 Tablet',
+                     Description:'Azee 500 Tablet is an antibiotic used to treat various types of bacterial infections of the respiratory tract, ear, nose, throat, lungs, skin, and eye in adults and children. It is also effective in typhoid fever and some sexually transmitted diseases like gonorrhea.
+                     ',
+                     Price:90,
+                     inCart:0
+                     },
+                     {
+                        id=10,
+                        Name:'Asthalin 100mcg Inhaler',
+                        Description:'Asthalin 100mcg Inhaler belongs to a group of medicines called fast-acting bronchodilators or “relievers”. It’s used to treat the symptoms of asthma and chronic obstructive pulmonary diseases (COPD) such as coughing, wheezing, and breathlessness.
+                        ',
+                        Price:105,
+                        inCart:0
+                        },
+                        {
+                           id=11,
+                           Name:'Amoxyclav 625 Tablet',
+                           Description:'Amoxyclav 625 Tablet is a penicillin-type of antibiotic that helps your body fight infections caused by bacteria. It is used to treat infections of the lungs (e.g., pneumonia), ear, nasal sinus, urinary tract, skin, and soft tissue. It will not work for viral infections such as the common cold.
+                           ',
+                           Price:113,
+                           inCart:0
+                           },
+                           {
+                              id=12,
+                              Name:'Azibact 500 Tablet',
+                              Description:'Azibact 500 Tablet is an antibiotic used to treat various types of bacterial infections of the respiratory tract, ear, nose, throat, lungs, skin, and eye in adults and children. It is also effective in typhoid fever and some sexually transmitted diseases like gonorrhea.
+                              ',
+                              Price: 67,
+                              inCart:0
+                             },
+
+]
 
 
 var data = [{
@@ -134,6 +224,7 @@ var data = [{
     const tempdata=[];
 
     let books=document.querySelectorAll('.book');
+    
     for(let i=0;i<books.length;i++)
     {
  books[i].addEventListener('click',()=>{
